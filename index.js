1b4823888d149e890cf037648b2aa60c3f05ef89
Rahul Edirisinghe <rahulediri@gmail.com>
2023-11-30 14:08:42 -0500

    fixes 4

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ let restify = require('restify')
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
 
-  server.listen(PORT, HOST, function () {
+  server.listen(PORT,/* HOST,*/ function () {
   console.log('Server %s listening at %s', server.name, server.url)
 });
 
